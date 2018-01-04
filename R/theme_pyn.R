498559f84286c0d865ea17f779d4a9275fd5316f
Joseph Ciesielski <jtcies@gmail.com>
2018-01-03 22:42:02 -0500

    adding DESCRIPTION file

--- R/theme_pyn.R
+++ R/theme_pyn.R
@@ -3,14 +3,14 @@
 #' A minimal theme for most charts that uses PYN's fonts
 #'
 
-theme_pyn<- function(base_family = "Century Gothic",
-                     base_size = 11,
-
-
-
-
-  text = element_text(family = 'Century Gothic'),
-                      axis.title.y = element_text(hjust = 1, vjust = 2),
-                      axis.title.x = element_text(hjust = 1),
-                      plot.title = element_text(hjust = 0))
+# theme_pyn<- function(base_family = "Century Gothic",
+#                      base_size = 11,
+#                      base_plot_title_size = 18,
+#                      base_
+#
+#
+#   text = element_text(family = 'Century Gothic'),
+#                       axis.title.y = element_text(hjust = 1, vjust = 2),
+#                       axis.title.x = element_text(hjust = 1),
+#                       plot.title = element_text(hjust = 0))
 
