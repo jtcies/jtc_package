a753eb550f63b0d23503bd7ec9a0fa3f848327f6
jtcies <jtcies@gmail.com>
2018-01-08 14:36:22 -0500

    moving pyn_pal to data/

--- R/pyn_colors.R
+++ R/pyn_colors.R
@@ -1,7 +1,45 @@
-#'
+#' Add PYN colors to a chart
+#' 
+#' 
+#' "PYN" theme has seven colors, the others have 4. If you need more than that,
+#' you will need to set theme directly using 
+#' scale_*_manual(values = pyn_pal[[XX]])
+#' 
+#' @param type "fill" for use with the fill aesthetic; 
+#' "color" for use with the color aesthetic
+#' @param scale "pyn" (default), "reds", "yellows", "blues", "greys"
+#' @param rev reverse the order of the colors; defaults to false
+#' @return a manual scale object
+#' 
 
-pyn_pallette <- c('#4f4f4f', '#c41520', '#f6bb42', '#3bb5a8', '#24cf5f', '#ff7800',
-                '#840e16', '#df9331', '#313131', '#05544b', '#15883d', '#e8530c',
-                '#ff4551', '#fff5ad', '#ececec', '#a5dad5', '#7effab', '#ffb368',
-                '#ffdddf', '#fffce3', '#fefcfb', '#e6fffd', '#dcffe8', '#ffe6cf')
+color_pyn <- function(type = "fill",
+                      scale = "pyn", 
+                      rev = FALSE) {
+  
+  key = list("pyn"      = pyn_pal[c(1:6)],
+             "reds"     = pyn_pal[c(19, 13, 1, 7)], 
+             "yellows"  = pyn_pal[c(20, 14, 2, 8)], 
+             "blues"    = pyn_pal[c(21, 15, 3, 9)], 
+             "greys"    = pyn_pal[c(22, 16, 4, 10)])
+
+  
+  if (type == "fill") {
+    if (!rev) {
+      ret <- scale_fill_manual(values = key[[scale]])
+    } else {
+      ret <- scale_fill_manual(values = rev(key[[scale]]))
+    }
+  }
+  
+  if (type == "color") {
+    if (!rev) {
+      ret <- scale_color_manual(values = key[[scale]])
+    } else {
+      ret <- scale_color_manual(values = rev(key[[scale]]))
+    }
+  }
+  
+  ret
+  
+}
 
